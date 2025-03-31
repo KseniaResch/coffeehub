369206048360fbfd5ebbb5034931ed2286459c9c
KseniaResch <kseniaresch@gmail.com>
2025-03-31 12:40:07 +0200

    Initial commit

---
+++ src/main/java/com/example/coffehub/CoffehubApplication.java
@@ -0,0 +1,13 @@
+package com.example.coffehub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoffehubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoffehubApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/coffehub/CoffehubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.coffehub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoffehubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
